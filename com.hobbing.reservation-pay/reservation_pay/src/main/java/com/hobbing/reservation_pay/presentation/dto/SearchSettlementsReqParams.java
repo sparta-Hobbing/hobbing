563276b7cc4986a501d471bb0fbff307c51de313
김성용 <tjddyd1565@naver.com>
2025-01-07 22:00:57 +0900

    feat - 정산 내역 조회 구현 완료

--- com.hobbing.reservation-pay/reservation_pay/src/main/java/com/hobbing/reservation_pay/presentation/dto/SearchSettlementsReqParams.java
+++ com.hobbing.reservation-pay/reservation_pay/src/main/java/com/hobbing/reservation_pay/presentation/dto/SearchSettlementsReqParams.java
@@ -3,14 +3,22 @@ package com.hobbing.reservation_pay.presentation.dto;
 
 import com.hobbing.reservation_pay.application.dto.SearchSettlementsDto;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Value;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
+
+@Value
+@Builder
 public class SearchSettlementsReqParams {
 
     @NotNull
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     LocalDateTime settledAfter;
     @NotNull
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     LocalDateTime settledBefore;
 
 

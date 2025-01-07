38e0b343013d7f2ca11d3942d6705e8f9f8aacd3
mo9mo9 <roseate920@naver.com>
2025-01-07 10:53:56 +0900

    coupon/setting 및 create

---
+++ com.hobbing.coupon/src/test/java/com/hobbing/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hobbing.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

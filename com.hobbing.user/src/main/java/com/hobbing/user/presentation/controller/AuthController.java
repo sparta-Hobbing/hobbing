2111fef4862f4c492964499ab886a902f2bb5b8a
Yeongho Kim <yeongho52@naver.com>
2025-01-21 19:18:58 +0900

    [fix] 로그확인 추가, Response값 변경

--- com.hobbing.user/src/main/java/com/hobbing/user/presentation/controller/AuthController.java
+++ com.hobbing.user/src/main/java/com/hobbing/user/presentation/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.hobbing.user.application.dto.response.PostAuthLoginResDto;
 import com.hobbing.user.application.service.AuthService;
 import com.hobbing.user.presentation.dto.PostAuthLoginReqDto;
 import com.hobbing.user.presentation.dto.PostAuthSignupReqDto;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -22,15 +25,15 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/signup")
-    public ApiResponse<Object> signup(@RequestBody @Valid PostAuthSignupReqDto dto){
-        log.info("signup");
+    public ApiResponse<Void> signup(@RequestBody @Valid PostAuthSignupReqDto dto) {
         authService.createUser(dto);
 
         return ApiResponse.ofSuccess(HttpStatus.CREATED, "회원가입했습니다.", null);
     }
 
     @PostMapping("/login")
-    public ApiResponse<PostAuthLoginResDto> login(@RequestBody @Valid PostAuthLoginReqDto dto){
+    public ApiResponse<PostAuthLoginResDto> login(@RequestBody @Valid PostAuthLoginReqDto dto, HttpServletRequest request){
+        log.info("url : " + request.getRequestURL());
         return ApiResponse.ofSuccess(HttpStatus.OK, "로그인했습니다.", authService.createAccessToken(dto));
     }
 

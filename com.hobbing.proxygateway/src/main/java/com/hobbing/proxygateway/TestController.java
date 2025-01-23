a6b79005d97714bd967e896af06272c5cd37ce3c
Yeongho Kim <yeongho52@naver.com>
2025-01-23 18:18:14 +0900

    [test] cicd테스트

--- com.hobbing.proxygateway/src/main/java/com/hobbing/proxygateway/TestController.java
+++ com.hobbing.proxygateway/src/main/java/com/hobbing/proxygateway/TestController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/")
+
     public ResponseEntity<Void> signup() {
         return ResponseEntity.ok(null);
     }

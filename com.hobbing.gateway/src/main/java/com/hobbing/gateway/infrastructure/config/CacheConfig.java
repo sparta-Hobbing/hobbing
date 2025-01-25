955433e896726cee28e61520647e757573181ae5
Yeongho Kim <yeongho52@naver.com>
2025-01-26 02:40:33 +0900

    [test] cicd테스트

---
+++ com.hobbing.gateway/src/main/java/com/hobbing/gateway/infrastructure/config/CacheConfig.java
@@ -0,0 +1,19 @@
+package com.hobbing.gateway.infrastructure.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("loadbalancer");
+        cacheManager.setCaffeine(com.github.benmanes.caffeine.cache.Caffeine.newBuilder()
+                .maximumSize(500)
+                .expireAfterAccess(300, java.util.concurrent.TimeUnit.SECONDS));
+        return cacheManager;
+    }
+}

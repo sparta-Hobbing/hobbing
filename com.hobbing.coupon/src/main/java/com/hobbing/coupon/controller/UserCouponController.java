aaae73bcbd475afe4c0e5c2f30d852a8768b09f0
Yeongho Kim <yeongho52@naver.com>
2025-01-27 04:43:25 +0900

    [feature] loadbalancer health check 예외 추가

--- com.hobbing.coupon/src/main/java/com/hobbing/coupon/controller/UserCouponController.java
+++ com.hobbing.coupon/src/main/java/com/hobbing/coupon/controller/UserCouponController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/user-coupons")
+@RequestMapping("/user-coupons")
 public class UserCouponController {
 
     private final UserCouponService userCouponService;

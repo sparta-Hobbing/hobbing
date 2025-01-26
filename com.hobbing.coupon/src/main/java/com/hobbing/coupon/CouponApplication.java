7d1de64a9d1c84e207915a068f00b4da0a8bffcd
Yeongho Kim <yeongho52@naver.com>
2025-01-27 02:53:47 +0900

    [feature] coupon 서비스 수정

--- com.hobbing.coupon/src/main/java/com/hobbing/coupon/CouponApplication.java
+++ com.hobbing.coupon/src/main/java/com/hobbing/coupon/CouponApplication.java
@@ -3,6 +3,7 @@ package com.hobbing.coupon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableE
 @SpringBootApplication
 public class CouponApplication {
 
